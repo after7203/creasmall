3fb2e2994591ac284c726063c62c89d9ddb528f2
Seoung Oh <after7203@naver.com>
2023-05-21 00:23:13 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -41,9 +41,7 @@ export default function Home() {
       }
     }
   `;
-  const { loading, error, data } = useQuery(GET_PRODUCTS, {
-    variables: { genderCode: "F" },
-  });
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
 
   useEffect(() => {
     console.log(data);
